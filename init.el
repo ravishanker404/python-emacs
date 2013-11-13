55976cd8b85210802d2fb79c029a64a101ebafe9
Ravi Shanker B <ravi@aplopio.com>
2013-11-13 18:47:51 +0530

    changed comment and uncomment shortcuts

--- init.el
+++ init.el
@@ -11,11 +11,24 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/sublime-themes/")
 (load-theme 'graham t)
 
-(global-set-key (kbd "M-m") 'comment-region)
-(global-set-key (kbd "M-n") 'uncomment-region)
+;;Toggle comment and uncomment
+;; Original idea from  http://www.opensubscriber.com/message/emacs-devel@gnu.org/10971693.html
+(defun toggle-comment-region (&optional arg)
+  "Replacement for the comment-dwim command.
+        If no region is selected and current line is not blank and we are not at the end of the line,
+        then comment current line.
+        Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line."
+  (interactive "*P")
+  (comment-normalize-vars)
+  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+      (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+    (comment-dwim arg)))
+(global-unset-key (kbd "C-?"))
+(global-set-key (kbd "C-?") 'toggle-comment-region)
+
 
 (load-file "~/.emacs.d/emacs-for-python/epy-init.el")
-(load-file "~/.emacs.d/packages.el")
+;; (load-file "~/.emacs.d/packages.el")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
